1ee9fd781db1f28b1c33abc0deccc66e29d1eec6
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-06-04 20:42:49 +0900

    GraphQL server作ったけど訳分からんerror吐いてる

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "app/article/client"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	ArticleClient *client.Client
+}
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"app/article/client"
 	"app/graph"
 	"app/graph/generated"
 	"log"
@@ -11,7 +12,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-const defaultPort = "8080"
+const defaultPort = "8081"
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,11 +20,30 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	// articleClientを生成
+	// articleClient, err := client.NewClient("localhost:50051")
+	articleClient, err := client.NewClient("go-grpc-server_app_1:50051")
+	// c, _ := client.NewClient("go-grpc-server_app_1:50051")
+	if err != nil {
+		// articleClient.Close()
+		log.Fatalf("Failed to create article client: %v\n", err)
+	}
 
+	// GraphQLサーバーに先程のResolverを実装
+	srv := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{
+				Resolvers: &graph.Resolver{
+					ArticleClient: articleClient,
+				}}))
+
+	// GraphQL playgroundのエンドポイント
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+
+	//　実装したクエリが実行可能なGraphQLサーバーのエンドポイント
 	http.Handle("/query", srv)
 
+	// GraphQLサーバーを起動
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
